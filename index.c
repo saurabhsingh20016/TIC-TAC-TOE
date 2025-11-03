5da09518f8cda2de75ee08e6319d9e506ec9275a
saurabhsingh20016 <saurabhsingh200712@gmail.com>
2025-11-03 12:37:52 +0530

    Refactor comments and enhance win/draw messages
    Updated comments for clarity and improved output messages.

--- index.c
+++ index.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdio.h>
 
 void displayBoard(char board[3][3]) {
     int i, j;
@@ -20,14 +19,14 @@ void displayBoard(char board[3][3]) {
 int checkWinner(char board[3][3]) {
     int i;
     for (i = 0; i < 3; i++) {
-        // Rows
+        // Check rows
         if (board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][0] != ' ')
             return 1;
-        // Columns
+        // Check columns
         if (board[0][i] == board[1][i] && board[1][i] == board[2][i] && board[0][i] != ' ')
             return 1;
     }
-    // Diagonals
+    // Check diagonals
     if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != ' ')
         return 1;
     if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] != ' ')
@@ -49,6 +48,7 @@ int main() {
             printf("Invalid input. Exiting.\n");
             return 1;
         }
+
         row--;
         col--;
 
@@ -62,13 +62,13 @@ int main() {
 
         if (checkWinner(board)) {
             displayBoard(board);
-            printf("Player %c wins!\n", currentPlayer);
+            printf("🎉 Player %c wins!\n", currentPlayer);
             break;
         }
 
         if (moves == 9) {
             displayBoard(board);
-            printf("It's a draw!\n");
+            printf("🤝 It's a draw!\n");
             break;
         }
 
@@ -77,50 +77,3 @@ int main() {
 
     return 0;
 }
-    if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != ' ')
-        return 1;
-    if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] != ' ')
-        return 1;
-
-    return 0;
-}
-
-int main() {
-    char board[3][3] = { {' ', ' ', ' '}, {' ', ' ', ' '}, {' ', ' ', ' '} };
-    char currentPlayer = 'X';
-    int row, col;
-    int moves = 0;
-
-    while (1) {
-        displayBoard(board);
-        printf("Player %c, enter row and column (1-3): ", currentPlayer);
-        scanf("%d %d", &row, &col);
-        row--;
-        col--;
-
-        if (row < 0 || row > 2 || col < 0 || col > 2 || board[row][col] != ' ') {
-            printf("Invalid move. Try again.\n");
-            continue;
-        }
-
-        board[row][col] = currentPlayer;
-        moves++;
-
-        if (checkWinner(board)) {
-            displayBoard(board);
-            printf("Player %c wins!\n", currentPlayer);
-            break;
-        }
-
-        if (moves == 9) {
-            displayBoard(board);
-            printf("It's a draw!\n");
-            break;
-        }
-
-        currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
-    }
-
-    return 0;
-}
-.
